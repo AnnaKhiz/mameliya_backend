d97e8eecd0af4f5bf67b4efb78dbfa27f75a9c2e
annakhizhniak <kukurika.anna@gmail.com>
2025-06-02 14:35:21 +0300

    [back] finished register endpoint. added password hash. added token in cookie. updated res statuses

--- services/authService.js
+++ services/authService.js
@@ -3,17 +3,56 @@ const knexConfig = require('../knexfile.js');
 const environment = process.env.NODE_ENV || 'development';
 const knex = knexLib(knexConfig[environment]);
 const { v4 : uuidv4 } = require('uuid');
-
+const { generateJWt, hashPass } = require('../utils/authEncoding');
+const isProd = process.env.NODE_ENV === 'production';
 async function signInUser(req, res, next) {
 	const { body: user } = req;
 
+	const { email, password } = user;
+
+	if (!email || !password) {
+		res.status(400).send({
+			result: false,
+			data: {},
+			code: 400,
+			message: 'Empty fields'
+		})
+	}
 	user.userId = uuidv4();
+	user.password = await hashPass(user.password);
+
 	try {
 		const result = await knex('users').insert(user).returning('*');
 		console.log(result);
-		res.send({ result: true, body: result})
+
+		req._auth = { role: 'user', userId: result[0].userId };
+		const token = generateJWt(req._auth);
+
+		res.cookie('token', token, {
+			httpOnly: true,
+			secure: isProd,
+			sameSite: isProd ? 'None' : 'Lax',
+			path: '/user',
+			expires: new Date(Date.now() + 86400000)
+		});
+
+		res.status(200).send({
+			result: true,
+			data: result[0],
+			code: 200,
+			message: 'Successful'
+		});
 	} catch (error) {
-		console.log('Error log in user', error)
+		if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+			res.status(409).send({
+				result: false,
+				data: {},
+				code: 409,
+				message: 'Email conflict'
+			})
+		} else {
+			console.log('Error [SIGN UP USER]: ', error)
+		}
 	}
 
 }
