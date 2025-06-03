aacee756873ad54b716f0e19232d22d18aba46db
annakhizhniak <kukurika.anna@gmail.com>
2025-06-03 15:53:11 +0300

    [back] added endpoint check is user authenticated

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { signUpUser, signInUser, logOutUser } = require('../services/authService');
+const { signUpUser, signInUser, logOutUser, checkIsTokenExpired } = require('../services/authService');
 const {parserJwt} = require("../middleware/authMiddleware");
 
 // GET
 router.post('/register', signUpUser);
 router.post('/login', signInUser);
 router.get('/logout', logOutUser);
+router.get('/check-auth', parserJwt, checkIsTokenExpired);
 
 module.exports = { router }
