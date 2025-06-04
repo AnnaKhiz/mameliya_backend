a30f8ce015065d50a0d38012fa52d10a204d532c
annakhizhniak <kukurika.anna@gmail.com>
2025-06-04 21:32:25 +0300

    [back] updated gitignore

---
+++ migrations/20250604165950_add_email_unique_again.js
@@ -0,0 +1,37 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+	await knex.schema.renameTable('users', 'users_old');
+
+	await knex.schema.createTable('users', table => {
+		table.string('first_name', 255).nullable();
+		table.string('last_name', 255).nullable();
+		table.string('email', 255).notNullable().unique();
+		table.string('password', 255).notNullable();
+		table.string('userId', 266).notNullable().primary();
+		table.integer('age', 2).nullable();
+	});
+
+	await knex('users').insert(knex.select('*').from('users_old'));
+
+	await knex.schema.dropTable('users_old');
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+	await knex.schema.renameTable('users', 'users_new');
+	await knex.schema.createTable('users', table => {
+		table.string('first_name', 255).notNullable();
+		table.string('last_name', 255).notNullable();
+		table.string('email', 255).notNullable();
+		table.string('password', 255).notNullable();
+		table.string('userId', 266).nullable();
+		table.integer('age', 2).nullable();
+	});
+	await knex.schema.dropTable('users_new');
+};
---
+++ migrations/20250604170332_mama_about.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+	return knex.schema.createTable('mama_about', (table) => {
+		table.increments().primary();
+		table.string('mood', 255).notNullable().defaultTo('good');
+		table.boolean('hasRituals').notNullable().defaultTo(false);
+		table.boolean('isTimerUsed').notNullable().defaultTo(false);
+		table.integer('timer').notNullable().defaultTo(5);
+
+		table.string('userId').notNullable()
+			.unique()
+			.references('userId')
+			.inTable('users')
+			.onDelete('CASCADE')
+	})
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('mama_about');
+};
