673140d04914d6678ec206643fccc29a3d4daed3
annakhizhniak <kukurika.anna@gmail.com>
2025-07-18 18:55:46 +0300

    [backend] check favorites list before adding new

--- services/dailyRitualsService.js
+++ services/dailyRitualsService.js
@@ -79,15 +79,29 @@ async function addToMyRituals(req, res, next) {
 	const { body: rituals } = req;
 
 	try {
-		const promises = rituals.map( (ritual) => {
-			const newRitual = {
-				user_id: userId,
-				ritual_id: ritual.id
-			}
-			return knex('favorite_rituals').insert(newRitual)
-		})
 
-		await Promise.all(promises);
+		const existingFavorites = await knex('favorite_rituals')
+			.where('user_id', userId)
+			.select('ritual_id');
+
+		const existingIds = new Set(existingFavorites.map(fav => fav.ritual_id));
+
+		const newFavorites = rituals.filter(ritual => !existingIds.has(ritual.id));
+
+		if (newFavorites.length === 0) {
+			return res.status(200).send({
+				result: false,
+				code: 204,
+				data: [],
+				message: 'All selected rituals already exist in favorites',
+			});
+		}
+		const insertData = newFavorites.map(ritual => ({
+			user_id: userId,
+			ritual_id: ritual.id,
+		}));
+
+		await knex('favorite_rituals').insert(insertData);
 
 		const favorites = await knex('daily_rituals as r')
 				.leftJoin('favorite_rituals as fav', 'r.id', 'fav.ritual_id')
@@ -107,7 +121,11 @@ async function addToMyRituals(req, res, next) {
 		res.send({ result: true, code: 200, data: result, message: 'Add to fav successfully'});
 	} catch (error) {
 		console.log('Error [ADD FAVORITE]', error);
-		res.send({ result: false, code: 500, data: [], message: 'Did not add'});
+		if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+			return res.send({ result: false, code: 409, data: [], message: 'Conflict, data already exist'});
+		} else {
+			res.send({ result: false, code: 500, data: [], message: 'Did not add'});
+		}
 	}
 }
 
@@ -135,9 +153,58 @@ async function getFavoriteRituals(req, res, next) {
 	}
 }
 
+async function removeRituals(req, res, next) {
+	const { userId } = req._auth;
+	const { body: rituals } = req;
+
+	try {
+		const deletePromises = rituals.map(async (ritual) => {
+			const ritualId = ritual.id;
+
+			const [ritualFromDB] = await knex('daily_rituals')
+				.where('id', ritualId)
+				.select('creator');
+
+			if (!ritualFromDB) return;
+
+			if (ritualFromDB.creator === userId) {
+				await knex('ritual_sections').where('ritual_id', ritualId).del();
+				await knex('daily_rituals').where('id', ritualId).del();
+				await knex('favorite_rituals').where('ritual_id', ritualId).del();
+			} else {
+				await knex('favorite_rituals')
+					.where({ user_id: userId, ritual_id: ritualId })
+					.del();
+			}
+		})
+
+		await Promise.all(deletePromises);
+
+		const result = await knex('daily_rituals as r')
+			.leftJoin('favorite_rituals as fav', 'r.id', 'fav.ritual_id')
+			.where('fav.user_id', userId)
+			.select(
+				'r.id',
+				'r.title',
+				'r.description',
+				'r.creator',
+				'r.created_at',
+				'r.cosmetic_name',
+			);
+
+
+		res.send({ result: true, code: 200, data: result, message: 'Get from fav successfully'});
+
+	} catch (error) {
+		console.log('Error [REMOVE FAVORITE]', error);
+		res.send({ result: false, code: 500, data: [], message: 'Did not remove'});
+	}
+}
+
 module.exports = {
 	addRitual,
 	getRitualsBySection,
 	addToMyRituals,
-	getFavoriteRituals
+	getFavoriteRituals,
+	removeRituals
 }
