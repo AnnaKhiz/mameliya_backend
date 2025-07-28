47fac14a17a62ccb3cadf1786c19260bb1fa3a55
annakhizhniak <kukurika.anna@gmail.com>
2025-07-28 18:43:01 +0300

    [backend] added field "photo" in the table "users". added logic for parsing uploaded file and saving it to the firebase storage

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { updateUserInfo } = require("../services/userService");
 const { parserJwt } = require("../middleware/authMiddleware");
+const { fileProcessingMiddleware, upload } = require('../middleware/parseUploadFileMiddware')
 
-router.patch('/update', parserJwt, updateUserInfo);
+router.patch('/update', parserJwt, upload.single('photo'), fileProcessingMiddleware, updateUserInfo);
 
 module.exports = { router }
