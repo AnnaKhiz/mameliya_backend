b5fd1812c3e984f2d422215ea3d567177e0b9b08
annakhizhniak <kukurika.anna@gmail.com>
2025-08-03 20:44:02 +0300

    [backend] added logger to the authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const { jwtKey } = require('config');
 const { verifyJwt } = require('../utils/authEncoding');
+const logger = require('../utils/logger')('auth-middleware');
 
 function parserJwt(req, res, next) {
 	const { token } = req.cookies;
@@ -7,8 +8,10 @@ function parserJwt(req, res, next) {
 	if (token) {
 		const payload = verifyJwt(token, jwtKey);
 		req._auth = payload;
+		logger.info(`${req.method} ${req.url} 200 JWT verified, added _auth info`);
 	} else {
 		req._auth = {};
+		logger.info(`${req.method} ${req.url} 200 JWT not verified, _auth info = {}`);
 	}
 	next();
 }
